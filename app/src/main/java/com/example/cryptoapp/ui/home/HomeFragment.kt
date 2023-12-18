daae8c2cc5b49d6973c67979e453889559b36dc6
Ferhat Çıkrık <ferhatckrk@gmail.com>
2023-12-19 00:43:52 +0300

    aded coin binding, homeRecyclerAdapter

--- app/src/main/java/com/example/cryptoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cryptoapp/ui/home/HomeFragment.kt
@@ -1,15 +1,16 @@
 package com.example.cryptoapp.ui.home
 
 
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.Observer
 import com.example.cryptoapp.base.BaseFragment
 import com.example.cryptoapp.databinding.FragmentHomeBinding
+import com.example.cryptoapp.model.home.Data
 import com.example.cryptoapp.utils.Constants.API_KEY
 import com.example.cryptoapp.utils.Constants.LIMIT
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.supervisorScope
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
@@ -19,6 +20,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
 
     override fun onCreateFinished() {
         viewModel.getData(API_KEY, LIMIT)
+
 
     }
 
@@ -31,7 +33,44 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
     }
 
     override fun observeEvents() {
+        with(viewModel) {
 
+            cyrptoResponse.observe(viewLifecycleOwner, Observer {
+                it?.let {
+                   it.data?.let { it1 -> setRecycler ( it1 ) }
+                }
+            })
+
+            isLoading.observe(viewLifecycleOwner, Observer{
+                 handleViews((it))
+            })
+
+            onError.observe(viewLifecycleOwner, Observer{
+                Toast.makeText(requireContext(), it, Toast.LENGTH_LONG ).show()
+            })
+
+
+        }
     }
+
+
+    private fun setRecycler(data: List<Data>) {
+        val mAdapter = HomeRecyclerAdapter(object : ItemClickListener {
+            override fun onItemClick(coin: Data) {
+                // TODO diger ekrana yolla
+
+            }
+
+        })
+        binding.rvHome.adapter = mAdapter
+        mAdapter.setList(data)
+    }
+
+
+    private fun handleViews(isLoading: Boolean = false) {
+        binding.rvHome.isVisible = !isLoading
+        binding.pbHome.isVisible = isLoading
+    }
+
 
 }
